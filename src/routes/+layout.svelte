da7a8a499547350d31f9baba9333700a16b2771d
Elias-Flood <elias.flood@yahoo.com>
2024-04-17 11:56:11 +0200

    Start using Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,19 @@
 	import '../app.pcss';
 </script>
 
-<slot />
+<div class="flex container">
+	<aside class="border border-2 border-solid h-[calc(100vh)] top-14 shrink-0">
+		<nav class="text-sm leading-5">
+			<div class="border border-2 ">
+				<img src="https://t4.ftcdn.net/jpg/00/89/55/15/360_F_89551596_LdHAZRwz3i4EM4J0NHNHy2hEUYDfXc0j.jpg"/>
+			</div>
+			<!-- 01 Side Menu -->
+			<div class="border border-2">
+				06 Article Body
+			</div>
+		</nav>
+	</aside>
+	<div class="grow">
+		<slot />
+	</div>
+</div>
