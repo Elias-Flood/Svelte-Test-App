80e2f6db9538189af7a36df54b44cb371dfe5143
Elias-Flood <elias.flood@yahoo.com>
2024-04-17 11:44:44 +0200

    Testing
    Testing

--- src/routes/archevist/+page.svelte
+++ src/routes/archevist/+page.svelte
@@ -83,7 +83,7 @@
 				<div>
 					<div class="border border-2">
 						<h2 class="text-lg p-2">Chapter Title</h2>
-						<p class="p-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p class="p-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Hello!</p>
 					</div>
 					<div class="border border-2 grid grid-flow-col grid-cols-5 h-[40%]">
 						<div class="col-span-2 p-4">
